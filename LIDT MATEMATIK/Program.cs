178b1c0828fa4006676209f76b61a3ee7be1c242
Rasmus <Rasmus@vinperlen.dk>
2021-01-25 10:40:11 +0100

    Add project files.

---
+++ LIDT MATEMATIK/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIDT_MATEMATIK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indsæt heltal (A)");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Indsæt heltal (B)");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Addition = {a + b}");
+            Console.WriteLine($"Division = {a / b}");
+            Console.WriteLine($"Potency {Math.Pow(a, b)}");
+            Console.ReadKey();
+        }
+    }
+}
