d52dfb63f0573190d8a3be3a2f168aba2f0957b5
Rasmus <Rasmus@vinperlen.dk>
2021-01-26 22:40:25 +0100

    methoder

--- LIDT MATEMATIK/Program.cs
+++ LIDT MATEMATIK/Program.cs
@@ -11,15 +11,35 @@ namespace LIDT_MATEMATIK
         static void Main(string[] args)
         {
             Console.WriteLine("Indsæt heltal (A)");
-            int a = Convert.ToInt32(Console.ReadLine());
+            int userInputA = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Indsæt heltal (B)");
-            int b = Convert.ToInt32(Console.ReadLine());
+            int userInputB = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine($"Addition = {a + b}");
-            Console.WriteLine($"Division = {a / b}");
-            Console.WriteLine($"Potency {Math.Pow(a, b)}");
+            Console.WriteLine($"Addition = {Addition(userInputA, userInputB)}");
+            Console.WriteLine($"Division = {Division(userInputA, userInputB)}");
+            Console.WriteLine($"Modulus = {Modulus(userInputA, userInputB)}");
+            Console.WriteLine($"Potency = {Potency(userInputA, userInputB)}");
+
             Console.ReadKey();
+        }
+
+        static int Addition(int userInputA, int userInputB)
+        {
+            return userInputA + userInputA;
+        }
+        static int Division(int userInputA, int userInputB)
+        {
+            return userInputA / userInputA;
+        }
+        static int Modulus(int userInputA, int userInputB)
+        {
+            return userInputA % userInputA;
+        }
+        static double Potency(int userInputA, int userInputB)
+        {
+            //Can't Convert 2^3!
+            return (int)Math.Pow(userInputB, userInputB);
         }
     }
 }
